f1f2e9d44705338713a21b22e9501e2fc6e90316
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2023-08-20 17:57:16 +0100

    Update.

--- themes/myquark/js/anchors.js
+++ themes/myquark/js/anchors.js
@@ -1,26 +1,28 @@
-// Find all h tags in the page
-const headers = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
+document.addEventListener("DOMContentLoaded", function() {
+    // Find all h tags in the page
+    const headers = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
 
-// Loop through each header and process them
-headers.forEach(header => {
-  // Create an ID from the header text
-  const id = header.textContent
-    .toLowerCase()
-    .replace(/\s+/g, '-')
-    .replace(/[^\w-]/g, '');
+    // Loop through each header and process them
+    headers.forEach(header => {
+    // Create an ID from the header text
+    const id = header.textContent
+        .toLowerCase()
+        .replace(/\s+/g, '-')
+        .replace(/[^\w-]/g, '');
 
-  // Add the ID to the header and set the data attribute
-  header.id = id;
-  header.setAttribute('data-anchor-processed', 'true');
+    // Add the ID to the header and set the data attribute
+    header.id = id;
+    header.setAttribute('data-anchor-processed', 'true');
 
-  // Create a link tag
-  const link = document.createElement('a');
-  link.setAttribute('data-anchor-icon', '#');
-  link.href = `#${id}`;
-  link.className = 'anchor-link';
+    // Create a link tag
+    const link = document.createElement('a');
+    link.setAttribute('data-anchor-icon', '#');
+    link.href = `#${id}`;
+    link.className = 'anchor-link';
 
-  // Add the link tag immediately after the header
-  header.parentNode.insertBefore(link, header.nextSibling);
+    // Add the link tag immediately after the header
+    header.parentNode.insertBefore(link, header.nextSibling);
+    });
 });
 
 document.body.addEventListener('click', (event) => {
