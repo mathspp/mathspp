6a0883fcd3edb40553ea4ef47d3f8d59ba741ef2
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2023-08-21 08:08:39 +0100

    Try creating plugin.

---
+++ plugins/mathspp-anchors/mathspp-anchors.php
@@ -0,0 +1,69 @@
+<?php
+namespace Grav\Plugin;
+
+use Composer\Autoload\ClassLoader;
+use Grav\Common\Data;
+use Grav\Common\Grav;
+use Grav\Common\Page\Interfaces\PageInterface;
+use Grav\Common\Plugin;
+use Grav\Common\Utils;
+use Grav\Plugin\PageToc\MarkupFixer;
+use Grav\Plugin\PageToc\TocGenerator;
+use RocketTheme\Toolbox\Event\Event;
+use Twig\TwigFunction;
+
+
+/**
+ * Class PageTOCPlugin
+ * @package Grav\Plugin
+ */
+class MathsppAnchorsPlugin extends Plugin
+{
+
+    /**
+     * @return array
+     *
+     * The getSubscribedEvents() gives the core a list of events
+     *     that the plugin wants to listen to. The key of each
+     *     array section is the event that the plugin listens to
+     *     and the value (in the form of an array) contains the
+     *     callable (or function) as well as the priority. The
+     *     higher the number the higher the priority.
+     */
+    public static function getSubscribedEvents(): array
+    {
+    return [
+        'onPluginsInitialized' => [
+            ['onPluginsInitialized', 0]
+        ]
+    ];
+    }
+
+    /**
+     * Initialize the plugin
+     */
+    public function onPluginsInitialized()
+    {
+        // Don't proceed if we are in the admin plugin
+        if ($this->isAdmin()) {
+            return;
+        }
+
+        // Enable the main event we are interested in
+        $this->enable([
+            'onPageContentProcessed'    => ['onPageContentProcessed', -20],
+        ]);
+    }
+
+    public function onPageContentProcessed(Event $event)
+    {
+        /** @var PageInterface $page */
+        $page = $event['page'];
+
+        if ($this->grav['config']->get('plugins.mathspp-anchors.include_css')) {
+            $this->grav['assets']->addCss('plugin://mathspp-anchors/assets/anchors.css');
+        }
+
+        $this->grav['assets']->addJs('plugin://mathspp-anchors/assets/anchors.js', ['group' => 'bottom', 'defer' => 'defer']);
+    }
+}
