a2f02db12460bb8abd67d20a60451cd0e983de3c
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2025-02-13 16:35:51 +0000

    Update

--- themes/myquark/js/theme_switcher.js
+++ themes/myquark/js/theme_switcher.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
     const themeToggleBtn = document.getElementById("theme-toggle");
+    const mobileThemeToggleBtn = document.getElementById("mobile-theme-toggle");
     const themeIcon = document.getElementById("theme-icon");
+    const mobileThemeIcon = document.getElementById("mobile-theme-icon");
     const currentTheme = localStorage.getItem("theme") || "dark";
 
     // Apply the saved theme
@@ -17,13 +19,18 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     themeToggleBtn.addEventListener("click", toggleTheme);
-    themeToggleBtn.addEventListener("touchend", toggleTheme);
+    if (mobileThemeToggleBtn) {
+        mobileThemeToggleBtn.addEventListener("click", toggleTheme);
+        mobileThemeToggleBtn.addEventListener("touchend", toggleTheme);
+    }
 
     function updateIcon(theme) {
         if (theme === "dark") {
             themeIcon.classList.replace("fa-moon", "fa-sun");
+            if (mobileThemeToggleBtn) mobileThemeIcon.classList.replace("fa-moon", "fa-sun");
         } else {
             themeIcon.classList.replace("fa-sun", "fa-moon");
+            if (mobileThemeToggleBtn) mobileThemeIcon.classList.replace("fa-sun", "fa-moon");
         }
     }
 });
