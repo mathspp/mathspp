e345e1ce047d22dc55ed2205616b8d5c8953981e
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2025-05-09 21:01:30 +0100

    Add logging to plugin

--- plugins/review-saver/review-saver.php
+++ plugins/review-saver/review-saver.php
@@ -18,6 +18,7 @@ class ReviewSaverPlugin extends Plugin
     {
         $action = $event['action'];
         if ($action !== 'review-saver') {
+            $this->grav['log']->info("action was {$action}");
             return;
         }
 
