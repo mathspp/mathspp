4baefb05965e61e32e8f67d585c956f8c06384fd
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2025-11-08 08:16:58 +0000

    Refine Python Vault snippet cards (#158)

--- themes/myquark/myquark.php
+++ themes/myquark/myquark.php
@@ -1,9 +1,22 @@
 <?php
 namespace Grav\Theme;
 
-use Grav\Common\Theme;
+use Grav\Theme\Myquark\Twig\PythonVaultTwigExtension;
 
 class Myquark extends Quark
 {
+    public static function getSubscribedEvents()
+    {
+        return parent::getSubscribedEvents() + [
+            'onTwigExtensions' => ['onTwigExtensions', 0],
+        ];
+    }
+
+    public function onTwigExtensions(): void
+    {
+        require_once __DIR__ . '/twig/PythonVaultTwigExtension.php';
+
+        $this->grav['twig']->twig()->addExtension(new PythonVaultTwigExtension());
+    }
 }
 ?>
