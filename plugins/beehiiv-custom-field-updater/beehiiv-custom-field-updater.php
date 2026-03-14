ad76ede09b31b7a1eef002fbf6ce488cbebf7b6a
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2026-03-14 18:34:28 +0000

    Fix name

--- plugins/beehiiv-custom-field-updater/beehiiv-custom-field-updater.php
+++ plugins/beehiiv-custom-field-updater/beehiiv-custom-field-updater.php
@@ -45,7 +45,7 @@ class BeehiivCustomFieldUpdaterPlugin extends Plugin
         $email = $this->grav['session']->__get('newsletter_subscriber.email');
 
         if (!$publicationId || !$token || !$email) {
-            $this->grav['log']->error("BeehiivCustomFieldUpdater: bad publication ID ({$pub_id}), token, or subscriber email ({$email}).");
+            $this->grav['log']->error("BeehiivCustomFieldUpdater: bad publication ID ({$publicationId}), token, or subscriber email ({$email}).");
             return;
         }
 
