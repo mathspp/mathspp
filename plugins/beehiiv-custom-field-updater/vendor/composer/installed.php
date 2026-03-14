846457eeafd60359e2525f53f4c273d7644c28e9
rodrigogiraoserrao <git-sync@trilby.media>
2026-03-14 18:33:36 +0100

    (Grav GitSync) Automatic Commit from rodrigogiraoserrao

---
+++ plugins/beehiiv-custom-field-updater/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'rodrigogiraoserrao/beehiiv-custom-field-updater',
+        'pretty_version' => '1.0.0',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'grav-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'rodrigogiraoserrao/beehiiv-custom-field-updater' => array(
+            'pretty_version' => '1.0.0',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'grav-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
